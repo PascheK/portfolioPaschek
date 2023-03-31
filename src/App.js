902271c5297962e4cb7807a6e1574f5ec9f3024b
PascheK <killian.pasche7@gmail.com>
2023-03-31 10:59:46 +0200

    Add analytics to website

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React from 'react'
 
-import {About, Footer, Header, Skills, Work, Futur} from './container';
-import {Navbar} from './components';
+import { About, Footer, Header, Skills, Work, Futur } from './container';
+import { Navbar } from './components';
 import { Helmet } from 'react-helmet-async';
+import { Analytics } from '@vercel/analytics/react';
 import './App.scss';
 
 const App = () => {
   return (
     <div className='app'>
-    <Helmet>
-      <title>Killian Portfolio | Full-Stack Developper</title>
-      <meta
-      name="description"
-      content="Explore Killian's portfolio, showcasing expertise in Web, App and more... Discover innovative projects, key skills, and my experiences for your needs."/>
-     <link rel='canonical' href='/'/>
-    </Helmet>
-    <Navbar/>
-      <Header/>
-      <About/>
+      <Helmet>
+        <title>Killian Portfolio | Full-Stack Developper</title>
+        <meta
+          name="description"
+          content="Explore Killian's portfolio, showcasing expertise in Web, App and more... Discover innovative projects, key skills, and my experiences for your needs." />
+        <link rel='canonical' href='/' />
+      </Helmet>
+      <Navbar />
+      <Header />
+      <About />
       <Futur />
-      <Work/>
-      <Skills/>
-      <Footer/>
+      <Work />
+      <Skills />
+      <Footer />
+      <Analytics/>
     </div>
+          
   );
 }
 
